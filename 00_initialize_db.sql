9cfe7477b6ca814b87eee7636768ecdbf48275a8
Paul Florence <paul.florence@makina-corpus.com>
2023-02-24 10:43:15 +0100

    Fix database creation

--- 00_initialize_db.sql
+++ 00_initialize_db.sql
@@ -1,5 +1,5 @@
 CREATE USER article password 'article';
-CREATE DATABASE article WITH OWNER=article ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' ;
+CREATE DATABASE article WITH OWNER=article ENCODING = 'UTF8' TEMPLATE template0;
 GRANT ALL PRIVILEGES ON DATABASE article TO article;
 
 CREATE EXTENSION ltree; -- l'extension ltree nous servira plus tard
