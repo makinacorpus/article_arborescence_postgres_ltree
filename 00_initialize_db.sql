72dc4542eaadef140836f18178d21b3fc2aa8066
Paul Florence <paul.florence@makina-corpus.com>
2023-03-06 11:18:57 +0100

    Use best practices when creating the database

--- 00_initialize_db.sql
+++ 00_initialize_db.sql
@@ -1,6 +1,5 @@
-CREATE USER article password 'article';
-CREATE DATABASE article WITH OWNER=article ENCODING = 'UTF8' TEMPLATE template0;
-GRANT ALL PRIVILEGES ON DATABASE article TO article;
+CREATE USER article WITH ENCRYPTED PASSWORD 'article';
+CREATE DATABASE article WITH OWNER article;
 
 CREATE EXTENSION ltree; -- l'extension ltree nous servira plus tard
 CREATE EXTENSION postgis;
